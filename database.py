d926fb3701414f62b45a392e3bc4fa61a04de4ff
Yammala syamkumar <yammalasyamkumar@gmail.com>
2026-07-30 15:21:41 +0530

    Change DATABASE_URL for Aiven MySQL connection
    Updated DATABASE_URL to connect to the new Aiven MySQL database.

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL="mysql+pymysql://root:1234@localhost:3306/projects_db"
+DATABASE_URL="mysql+pymysql://avnadmin:AVNS_GP8uTeG-MEbQfk0__mq@mysqldb-yammalasyamkumar-backend.f.aivencloud.com:17963/defaultdb"
 engine=create_engine(DATABASE_URL)
 
 SessionLocal=sessionmaker(
